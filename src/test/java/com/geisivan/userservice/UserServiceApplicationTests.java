8696874939141d8c28a7a6e12b44510661f0c463
Geisivan <gsv1205@yahoo.com>
2026-05-03 18:51:31 -0300

    initial commit

---
+++ src/test/java/com/geisivan/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geisivan.userservice;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
