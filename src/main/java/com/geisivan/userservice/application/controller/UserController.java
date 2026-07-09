d4b661899221366fc2e2de1ff0491cda8ad2b2d2
Geisivan <gsv1205@yahoo.com>
2026-07-09 20:21:48 -0300

    feat(user): add findAuthenticatedUser endpoint

---
+++ src/main/java/com/geisivan/userservice/application/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.geisivan.userservice.application.controller;
+
+import com.geisivan.userservice.application.dto.response.UserResponseDTO;
+import com.geisivan.userservice.application.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users/me")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<UserResponseDTO> findAuthenticatedUser(){
+
+        UserResponseDTO response = userService.findAuthenticatedUser();
+
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/geisivan/userservice/application/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.geisivan.userservice.application.service.impl;
+
+import com.geisivan.userservice.application.dto.response.UserResponseDTO;
+import com.geisivan.userservice.application.mapper.UserMapper;
+import com.geisivan.userservice.application.service.UserService;
+import com.geisivan.userservice.domain.entity.User;
+import com.geisivan.userservice.infrastructure.exception.custom.ResourceNotFoundException;
+import com.geisivan.userservice.infrastructure.repository.UserRepository;
+import com.geisivan.userservice.infrastructure.security.util.AuthenticatedUserUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDTO findAuthenticatedUser() {
+        Long userId = AuthenticatedUserUtil.getId();
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "User not found"));
+
+        return userMapper.toDTO(user);
+    }
+}
