5c8ff2986d03583a53cff45323584a6d0317f55d
Geisivan <gsv1205@yahoo.com>
2026-07-16 19:53:13 -0300

    feat(admin): add find all users endpoint

--- src/main/java/com/geisivan/userservice/application/service/AdminUserService.java
+++ src/main/java/com/geisivan/userservice/application/service/AdminUserService.java
@@ -2,8 +2,12 @@ package com.geisivan.userservice.application.service;
 
 import com.geisivan.userservice.application.dto.request.AdminUserRequestDTO;
 import com.geisivan.userservice.application.dto.response.UserResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface AdminUserService {
 
     UserResponseDTO createUser(AdminUserRequestDTO dto);
+
+    Page<UserResponseDTO> findAllUsers(Pageable pageable);
 }
